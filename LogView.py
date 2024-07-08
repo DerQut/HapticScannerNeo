f77e01ca673a8ce3fc29e24d84ab9da6fc6a6360
Marcel Chołodecki <cholodecki@gmail.com>
2024-07-08 22:14:05 +0200

    Preparations for a LogView to exist

--- LogView.py
+++ LogView.py
@@ -11,3 +11,10 @@ from Color import *
 class LogView(QWidget):
     def __init__(self):
         super().__init__()
+        self.vStack = QVBoxLayout()
+
+        self.vStack.setContentsMargins(0, 0, 0, 0)
+        self.vStack.setSpacing(0)
+        self.vStack.addWidget(Color(MacColoursDark.red))
+
+        self.setLayout(self.vStack)
--- NavigationSplitView.py
+++ NavigationSplitView.py
@@ -20,6 +20,7 @@ class NavigationSplitView(QWidget):
         self.sidebarView = sidebarView
         self.detailView = detailView
         self.logView = LogView()
+        self.logView.setFixedHeight(100)
 
         self.cvHStack = QHBoxLayout()
 
@@ -30,40 +31,75 @@ class NavigationSplitView(QWidget):
         self.sidebarToggleButton.setFixedSize(25, 50)
         self.sidebarToggleButton.clicked.connect(self.toggleSidebar)
 
-        toggleVStack = QVBoxLayout()
-        toggleVStack.addWidget(self.sidebarToggleButton)
-        toggleVStack.setContentsMargins(0, 0, 0, 0)
-        toggleSpacerDown = Color(assets.MacColoursDark.bg_colour)
-        toggleSpacerDown.setFixedHeight(800-50)
-        toggleVStack.addWidget(toggleSpacerDown)
+        sidebarToggleVStack = QVBoxLayout()
+        sidebarToggleVStack.addWidget(self.sidebarToggleButton)
+        sidebarToggleVStack.setContentsMargins(0, 0, 0, 0)
+        sidebarToggleSpacerDown = Color(assets.MacColoursDark.bg_colour)
+        sidebarToggleSpacerDown.setFixedHeight(800 - 50)
+        sidebarToggleVStack.addWidget(sidebarToggleSpacerDown)
 
         self.cvHStack.addWidget(self.sidebarView)
-        self.cvHStack.addLayout(toggleVStack)
+        self.cvHStack.addLayout(sidebarToggleVStack)
 
-        self.cvHStack.addWidget(self.detailView)
+        self.dvVStack = QVBoxLayout()
+        self.dvVStack.setContentsMargins(0, 0, 0, 0)
+        self.dvVStack.setSpacing(0)
+        self.dvVStack.addWidget(self.detailView)
+
+        self.logToggleButton = QPushButton("▼")
+        self.logToggleButton.setFixedSize(50, 25)
+        self.logToggleButton.clicked.connect(self.toggleLog)
+
+        logToggleHStack = QHBoxLayout()
+        logToggleHStack.setContentsMargins(0, 0, 0, 0)
+        logToggleHStack.setSpacing(0)
+        self.logToggleSpacerLeft = Color(assets.MacColoursDark.bg_colour)
+        self.logToggleSpacerLeft.setFixedWidth(800-50)
+        logToggleHStack.addWidget(self.logToggleSpacerLeft)
+        logToggleHStack.addWidget(self.logToggleButton)
+        self.dvVStack.addLayout(logToggleHStack)
+        self.dvVStack.addWidget(self.logView)
+
+        self.cvHStack.addLayout(self.dvVStack)
 
         self.setLayout(self.cvHStack)
 
-        self.timer = QTimer()
-        self.timer.setInterval(1)
-        self.timer.timeout.connect(self.animateSidebar)
+        self.sidebarTimer = QTimer()
+        self.sidebarTimer.setInterval(1)
+        self.sidebarTimer.timeout.connect(self.animateSidebar)
+
+        self.logTimer = QTimer()
+        self.logTimer.setInterval(1)
+        self.logTimer.timeout.connect(self.animateLog)
 
         self.sidebarToggleButton.setStyleSheet("background-color: rgba(37,38,40,0);")
+        self.logToggleButton.setStyleSheet("background-color: rgba(37,38,40,0);")
 
         #######
 
     def toggleSidebar(self):
         self.isSideBarVisible = not self.isSideBarVisible
         self.sidebarToggleButton.setText("◀" if self.isSideBarVisible else "▶")
-        self.timer.start()
+        self.sidebarTimer.start()
+
+    def toggleLog(self):
+        self.isLogViewVisible = not self.isLogViewVisible
+        self.logToggleButton.setText("▼" if self.isLogViewVisible else "▲")
+        self.logTimer.start()
 
     def animateSidebar(self):
         if self.sidebarView.width() != self.isSideBarVisible * 400:
             self.sidebarView.setFixedWidth(self.sidebarView.width()-10+20*self.isSideBarVisible)
+            self.logToggleSpacerLeft.setFixedWidth(self.logToggleSpacerLeft.width()+10-20*self.isSideBarVisible)
         else:
-            self.timer.stop()
+            self.sidebarTimer.stop()
+
+    def animateLog(self):
+        if self.logView.height() != self.isLogViewVisible * 100:
+            self.logView.setFixedHeight(self.logView.height()-5+10*self.isLogViewVisible)
+        else:
+            self.logTimer.stop()
 
     def changeDetailView(self, newDetailView: QWidget):
-        self.cvHStack.removeWidget(self.detailView)
+        self.dvVStack.replaceWidget(self.detailView, newDetailView)
         self.detailView = newDetailView
-        self.cvHStack.addWidget(self.detailView)
