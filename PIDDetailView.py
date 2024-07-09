145f2a8f5e5106ad2744aa06804b107207a0316d
Marcel Chołodecki <cholodecki@gmail.com>
2024-07-10 00:46:09 +0200

    Added a TODO list

--- PIDDetailView.py
+++ PIDDetailView.py
@@ -11,6 +11,11 @@ class PIDDetailView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # TODO: Three numeric inputs (Kp, Ki, Kd) with ranges of 0-65535? gotta ask the doctor
+        # TODO: Manual vs Online exclusive checkbox (what does it do?)
+        # TODO: CE and CV readout (Cross-Entropy? or like the Error? idk and Control Variable, which is the output of the PID summator)
+        # TODO Setpoint numeric input (from -10V to +10V?)
+
         vStack = QVBoxLayout()
         vStack.setContentsMargins(0, 0, 0, 0)
         vStack.setSpacing(0)
